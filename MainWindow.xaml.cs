ab49c11d3774d30431591e1651cd0bff644fd361
student <student@PC-43-239-07>
2023-04-04 14:41:46 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MarathonSkills
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public string Time
+        {
+            get
+            {
+                DateTime dt1 = DateTime.Now;
+                DateTime dt2 = DateTime.Parse("2023-5-1 6:00");
+
+                TimeSpan ts = dt2 - dt1;
+
+                return string.Format("{0} дн {1} ч {2} мин {3} сек до старта марафона!", ts.Days, ts.Hours, ts.Minutes, ts.Seconds);
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Timer tmr = new Timer();
+
+            tmr.Interval = 1000;
+            tmr.Elapsed += Tmr_Elapsed;
+
+            tmr.Start();
+        }
+
+        private void Tmr_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            PropertyChange("Time");
+        }
+
+        private void PropertyChange(string name)
+        {
+            if(PropertyChanged != null) 
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
